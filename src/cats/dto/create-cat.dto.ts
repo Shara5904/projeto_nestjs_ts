201aaab2113f1735cf857787bec72808627749f4
shara5904 <srss1@aluno.ifal.edu.br>
2023-09-05 16:26:25 -0300

    .

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,10 +1,11 @@
-import { IsNotEmpty, MinLength } from "class-validator";
+import { IsNotEmpty, MinLength, NotEquals } from "class-validator";
 
 export class CreateCatDto {
     id: number;
 
     @IsNotEmpty()
-    @MinLength(2)
+    @MinLength(2, {message: 'O nome do gato tem que ter no mínimo 2 caracters.'})
+    @NotEquals(" ")
     nome: string;
     raca: string;
     corOlhos: string;
