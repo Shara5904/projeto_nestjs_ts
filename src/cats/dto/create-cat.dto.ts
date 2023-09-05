8ab8dbd8322905b1bf636806d0f0c6b1770bbcec
shara5904 <srss1@aluno.ifal.edu.br>
2023-09-05 15:47:15 -0300

    .

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,5 +1,10 @@
-export class CreateCatDto{
+import { IsNotEmpty, MinLength } from "class-validator";
+
+export class CreateCatDto {
     id: number;
+
+    @IsNotEmpty()
+    @MinLength(2)
     nome: string;
     raca: string;
     corOlhos: string;
