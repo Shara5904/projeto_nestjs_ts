1bebce147a98fc0b0bd019272b50c9d758af60b3
Shara5904 <128625731+Shara5904@users.noreply.github.com>
2023-09-19 16:31:40 -0300

    configuração inicial de acesso ao banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from  '@nestjs/typeorm';import { DataSource } from 'typeorm';
+;
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dpg-ck4uieui9prc73akurf0-a.oregon-postgres.render.com',
+      port: 5432,
+      username: 'shara_user',
+      password: '5904',
+      database: 'shara',
+      entities: [],
+      synchronize: true,
+      ssl: true,
+    })],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ src/cats/cats.module.ts
@@ -0,0 +1,9 @@
+import { Module, Controller } from '@nestjs/common';
+import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
+
+@Module({
+    controllers: [CatsController],
+    providers: [CatsService],
+})
+export class CatsModule {}
